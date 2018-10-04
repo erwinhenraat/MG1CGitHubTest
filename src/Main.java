cac680e739fe1a28a84169a92e2285855404b118
erwinhenraat <e.henraat@ma-web.nl>
2018-10-04 12:18:29 +0200

    if else en scanner voorbeelden

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,33 @@
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello MG1C!");
-        System.out.println("Hoe is het met jullie?");
+
+        //statement
+        int a;
+
+        boolean rijbewijsBehaald =true;
+        //Operator
+        //Operation
+        Scanner s = new Scanner(System.in);
+        a = s.nextInt();
+
+        //Conditional
+        //if(){}else if(){}else{}
+
+        if(a > 50){
+            System.out.println("hallo");
+        }else if(a == 5){
+            System.out.println("hoi");
+        }else if(a == 6){
+            System.out.println("doei");
+        }else if(a == 18){
+            if(rijbewijsBehaald){
+                System.out.println("gefeliciteerd met je rijbewijs");
+            }
+
+        }
+
 
     }
 }
