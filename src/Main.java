e28a150365d18c05fe8b58b8a19cc5c0915962a2
erwinhenraat <e.henraat@ma-web.nl>
2018-10-17 12:08:26 +0200

    voorbeeld forloop

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,26 @@
-import javax.swing.plaf.synth.SynthLookAndFeel;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        String[] names = {
+                "Henk",
+                "Kees",
+                "Frits",
+                "Hans",
+                "Bertje"};
 
-        //for loop
-        //een aantal keer
-        //Ik wil 10 x mijn naam invullen
 
-        String[] names = {"Piet", "Klaas", "Jan", "Kees", "Barry", "Hendrik", "Annette", "Jaqueline", "Harry"};
-
-/*
-        for(int i = 0; i < names.length; i++){
+        //x aantal keer code uitvoert
+        for(int i=0;i<names.length;i++){
             System.out.println(names[i]);
         }
 
-        for(String item : names){
+        for (String item : names){
             System.out.println(item);
         }
-*/
-        System.out.println("Wat is je naam? ");
-        Scanner s = new Scanner(System.in);
-        String name = s.next();
-
-        for(int i = name.length()-1; i >= 0 ; i--){
-            System.out.println("Hallo ");
-            System.out.println(name.charAt(i));
-        }
 
 
-
-        while(!name.equals("Erwin")){
-            System.out.println("je bent erwin niet,  vul je naam in");
-            name = s.next();
-        }
-
-        System.out.println("Hoi Erwin");
-
-
-        do{
-
-        }while();
-
-
-
-        //while loop
-        //voorwaardelijk
-        //ik wil mijn naam invullen zolang deze niet goed is ingevuld
-
+        //code uitvoert zolang iets waar is
 
     }
 }
